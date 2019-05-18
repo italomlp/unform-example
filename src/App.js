cda380ac0fa16c07042e166783a23bfeede89340
Italo Menezes <italomlporoca@hotmail.com>
2019-05-18 15:45:47 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from "react";
+
+import { Input, Form, Scope, useField } from "@rocketseat/unform";
+import * as Yup from "yup";
+
+import ReactDatepicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+const schema = Yup.object().shape({
+  fullname: Yup.string()
+    .min(5, "The FULL name is only this?")
+    .required("Full name is required."),
+  phone: Yup.string()
+    .matches(/^[0-9]{9}$/g, "Is this a phone number?")
+    .required("Phone is required."),
+  email: Yup.string().email("Is this an email?"),
+  birthDate: Yup.date().notRequired(),
+  address: Yup.object().shape({
+    city: Yup.string().notRequired(),
+    state: Yup.string().notRequired()
+  })
+});
+
+const Datepicker = ({ name, label }) => {
+  const ref = useRef(null);
+  const { fieldName, registerField, defaultValue, error } = useField(name);
+  const [selectedDate, setSelectedDate] = useState(defaultValue);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: ref.current,
+      path: "props.selected",
+      clearValue: pickerRef => {
+        console.log("ran clearValue");
+        pickerRef.clear();
+      }
+    });
+  }, [fieldName]); // eslint-disable-line
+
+  return (
+    <>
+      {!!label && <label>{label}</label>}
+      <ReactDatepicker
+        name={fieldName}
+        selected={selectedDate}
+        onChange={date => setSelectedDate(date)}
+        ref={ref}
+      />
+      {error && <span>{error}</span>}
+    </>
+  );
+};
 
 function App() {
+  // const initialData = {
+  //   fullname: "Italo Menezes",
+  //   phone: "999999999",
+  //   email: "italo@email.com",
+  //   address: {
+  //     city: "Rio de Janeiro",
+  //     state: "Rio de Janeiro"
+  //   }
+  // };
+
+  const handleSubmit = (data, { resetForm }) => {
+    console.log(data);
+    resetForm();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Contact form</h1>
+
+      <Form onSubmit={handleSubmit} schema={schema}>
+        <Input className="input" name="fullname" label="Full name" />
+        <Input className="input" name="phone" label="Phone" />
+        <Input className="input" name="email" label="Email" />
+
+        <Datepicker name="birthDate" label="Birth date" />
+
+        <Scope path="address">
+          <Input name="city" label="City" />
+          <Input name="state" label="State" />
+        </Scope>
+
+        <button type="submit">Save</button>
+      </Form>
     </div>
   );
 }
